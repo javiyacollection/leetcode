8ee6fe89594570eb5084cfc1e3fb43f56e4a4842
Jaykumar Javiya <39582016+javiyacollection@users.noreply.github.com>
2022-01-02 23:57:10 +0100

    Update LongestCommonPrefix.java

--- leetocode/src/LongestCommonPrefix.java
+++ leetocode/src/LongestCommonPrefix.java
@@ -1,34 +1,19 @@
 class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        
+       
         String firstString= strs[0];
-        if(firstString==""){
-            return "";
-        }
-
-        String commonString="";
-        boolean isExist= true;
         char[] chararray =firstString.toCharArray();
-
         for(int i=0; i<chararray.length; i++){
             for(int j=1; j<strs.length; j++){
                 String str=strs[j];
-                if(str.length()<i+1){
-                    isExist=false;
-                    break;
-                }
-                if(!(firstString.charAt(i)==str.charAt(i))){
-                    isExist=false;
-                    break;
+                if(str.length()<i+1 || firstString.charAt(i)!=str.charAt(i)){
+                   return firstString.substring(0,i);
+                    
                 }
             }
-            if(isExist){
-                commonString=commonString+chararray[i];
-            }else{
-                break;
-            }
+            
         }
-        return commonString;
+        return firstString;
     }
     
 }
